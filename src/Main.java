531484e6dae58277eb0019a9eea3cf6321138bdb
ozkan cil <ozkancil92@gmail.com>
2022-12-11 13:23:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+long nFactorial=factorialFunction(5);
+        System.out.println(nFactorial);
+       // System.out.println(factorialFunction(5));
+    }
+    public static long factorialFunction(long n){
+        if(n<=1) return 1;
+        return n*factorialFunction(n-1);
+    }
+}
